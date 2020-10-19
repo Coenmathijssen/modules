f13e272922cee84e1334ee24eb81e8008b3f2143
Coen Mathijssen <mathijssen.coen@gmail.com>
2020-10-19 14:16:13 +0200

    v1 finished

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import {TimelineMax, TweenMax, Power2 } from 'gsap/all'
-// import { Tween } from 'gsap/gsap-core'
 
 let timeOut
+let totalPrice = 0
 
 let addToCartButtons = document.querySelectorAll('.item__add-to-cart')
 addToCartButtons = Array.from(addToCartButtons)
@@ -16,10 +16,12 @@ if (addToCartButtons.length > 0) {
 
 function addToBasket (target) {
     // Selecting the right elements (images and names of shoes) and cloning them
-    const image = target.parentNode.previousElementSibling.firstElementChild
-    const name = target.parentNode.previousElementSibling.previousElementSibling.firstElementChild 
+    const image = target.parentNode.parentNode.querySelector('.item__image-container').querySelector('img')
+    const name = target.parentNode.parentNode.querySelector('.item__text-container').querySelector('.item__product-name')
+    const price = target.parentNode.parentNode.querySelector('.item__text-container').querySelector('.item__product-price')
     const newImage = image.cloneNode(true)
     const newName = name.cloneNode(true)
+    const newPrice = price.cloneNode(true)
     const floatingImage = image.cloneNode(true)
 
     // Create div, give the right class and append the chosen image to it
@@ -32,26 +34,34 @@ function addToBasket (target) {
     const shoppingCartSmall = document.querySelector('.shopping_cart_small')
     const shoppingCartSmallHeight = shoppingCartSmall.offsetHeight
 
+    TweenMax.to('.shopping_cart_large', 0.5, { y: '100%', ease: Power2.easeOut })
     TweenMax.to('.products_overview', 0.5, { y: '-' + (shoppingCartSmallHeight - 40), ease: Power2.easeOut })
 
+    // Wait for 4 seconds to animate shopping cart small out. Reset this timer if another item is added
     clearTimeout(timeOut)
     
     timeOut = setTimeout(() => {
         TweenMax.to('.products_overview', 0.5, { y: 0, ease: Power2.easeOut })
     }, 4000);
     
-    // Add item to shopping cart
+    // Add item to shopping cart small
     appendToShoppingCartSmall(item)
+
+    // Add item to shopping cart large
+    appendToShoppingCartLarge(newImage, newName, newPrice)
+
+    // update total price
+    updateTotalPrice(newPrice)
 
     // Animate
     const fivePlus = document.querySelector('.five-plus')
-    fivePlus ? animate(fivePlus, image, floatingImage) : animate(item, image, floatingImage)
+    fivePlus ? animateImage(fivePlus, image, floatingImage) : animateImage(item, image, floatingImage)
 
-    // Update total
-    updateTotal()
+    // Update total items
+    updateTotalItems()
 } 
 
-function animate (item, image, floatingImage) {
+function animateImage (item, image, floatingImage) {
     // Adding the right styling
     floatingImage.classList.add('floating-image')
 
@@ -95,7 +105,7 @@ function appendToShoppingCartSmall (item) {
     }
 }
 
-function updateTotal () {
+function updateTotalItems () {
     let shoppingCartItems = document.querySelectorAll('.shopping_cart_small__item')
     shoppingCartItems = Array.from(shoppingCartItems)
 
@@ -110,4 +120,130 @@ function removeElement(element) {
     return function() {
       element.parentNode.removeChild(element);
     };
-  }
+}
+
+function appendToShoppingCartLarge (image, name, price) {
+    const shoppingCartLarge = document.querySelector('.shopping_cart_large__items')
+
+    if (shoppingCartLarge) {
+        const newImage = image.cloneNode(true)
+        let item = document.createElement('div')
+        item.classList.add('item')
+        let left = document.createElement('div')
+        left.classList.add('left')
+        let imageContainer = document.createElement('div')
+        imageContainer.classList.add('image-container')
+        
+        imageContainer.appendChild(newImage)
+        left.appendChild(imageContainer)
+        left.appendChild(name)
+        item.appendChild(left)
+        item.appendChild(price)
+
+        shoppingCartLarge.appendChild(item)
+    }
+}
+
+function updateTotalPrice (price) {
+    if (price) {
+        price = convertToNumber(price.textContent)
+        price > 0 ? totalPrice = roundToTwoDecimals(totalPrice, price): totalPrice = totalPrice
+        document.querySelector('.total').textContent = totalPrice
+    }
+}
+
+function convertToNumber (string) {
+    const number = parseFloat(string.replace(/,/g, '.'))
+    return number
+}
+
+function roundToTwoDecimals (numberOne, numberTwo) {
+    let roundedNumber = Math.round(((numberOne + numberTwo) + Number.EPSILON) * 100) / 100
+    return roundedNumber
+}
+
+// Listen for swipe events
+const shoppingCartSmall = document.querySelector(".shopping_cart_small")
+const shoppingCartLarge = document.querySelector(".shopping_cart_large")
+
+shoppingCartSmall.addEventListener("touchstart", startTouch, false)
+shoppingCartSmall.addEventListener("touchmove", moveTouch, false)
+shoppingCartLarge.addEventListener("touchstart", startTouch, false)
+shoppingCartLarge.addEventListener("touchmove", moveTouch, false)
+
+let initialX = null
+let initialY = null
+
+function startTouch(e) {
+    initialX = e.touches[0].clientX
+    initialY = e.touches[0].clientY
+}
+
+function moveTouch(e) {
+    let swipe
+
+    if (initialX === null) {
+      return
+    }
+
+    if (initialY === null) {
+      return
+    }
+
+    const currentX = e.touches[0].clientX
+    const currentY = e.touches[0].clientY
+
+    const diffX = initialX - currentX
+    const diffY = initialY - currentY
+
+    if (Math.abs(diffX) > Math.abs(diffY)) {
+      // sliding horizontally
+      if (diffX > 0) {
+        console.log("swiped left")
+        swipe = 'left'
+      } else {
+        console.log("swiped right")
+        swipe = 'right'
+      }  
+    } else {
+      // sliding vertically
+      if (diffY > 0) {
+        console.log("swiped up")
+        swipe = 'up'
+        showShoppingCartLarge()
+      } else {
+        console.log("swiped down")
+        swipe = 'down'
+        hideShoppingCartLarge()
+      }  
+    }
+
+    initialX = null
+    initialY = null
+    
+    return swipe
+};
+
+function showShoppingCartLarge () {
+    const shoppingCartLarge = document.querySelector('.shopping_cart_large')
+    const shoppingCartLargeHeight = shoppingCartLarge.offsetHeight
+
+    TweenMax.to(shoppingCartLarge, 0.5, { y: 0, ease: Power2 })
+    TweenMax.to('.products_overview', 0.4, { y: '-' + (shoppingCartLargeHeight - 40), delay: 0.3, ease: Power2.easeOut })
+
+    clearTimeout(timeOut)
+}
+
+function hideShoppingCartLarge () {
+    const shoppingCartSmall = document.querySelector('.shopping_cart_small')
+    const shoppingCartSmallHeight = shoppingCartSmall.offsetHeight
+    
+    TweenMax.to('.products_overview', 0.5, { y: '-' + (shoppingCartSmallHeight - 40), ease: Power2.easeOut })
+    TweenMax.to('.shopping_cart_large', 0.5, { y: '100%', ease: Power2 })
+
+    clearTimeout(timeOut)
+    
+    timeOut = setTimeout(() => {
+        TweenMax.to('.products_overview', 0.5, { y: 0, ease: Power2.easeOut })
+    }, 4000)
+}
